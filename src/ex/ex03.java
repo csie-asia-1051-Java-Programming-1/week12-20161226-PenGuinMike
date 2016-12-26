94f824d0a3c0de46f3064ff0723d1d3373f2464d
PenGuinMike <104021074@live.asia.edu.tw>
2016-12-26 15:51:38 +0800

    OOXX
    沒設限制

--- src/ex/ex03.java
+++ src/ex/ex03.java
@@ -4,12 +4,54 @@
  * Date: 2016/12/26
  * Author: 1050210XX 周永振老師
  */
-
+import java.util.*;
 public class ex03 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scn = new Scanner(System.in);
+		int data [][]=new int [3][3];
+		for(int i=0;i<3;i++){
+			for(int j=0;j<3;j++){
+				data[i][j]=scn.nextInt();
+			}
+		}
+		if(data[0][0]+data[0][1]+data[0][2]==0){
+			System.out.println("plater 0 Win !");
+		}else if(data[1][0]+data[1][1]+data[1][2]==0){
+			System.out.println("plater 0 Win !");
+		}else if(data[2][0]+data[2][1]+data[2][2]==0){
+			System.out.println("plater 0 Win !");
+		}else if(data[0][2]+data[1][1]+data[2][0]==0){
+			System.out.println("plater 0 Win !");
+		}else if(data[0][0]+data[1][1]+data[2][2]==0){
+			System.out.println("plater 0 Win !");
+		}else if(data[0][0]+data[1][0]+data[2][0]==0){
+			System.out.println("plater 0 Win !");
+		}else if(data[0][1]+data[1][1]+data[2][1]==0){
+			System.out.println("plater 0 Win !");
+		}else if(data[0][2]+data[1][2]+data[2][2]==0){
+			System.out.println("plater 0 Win !");
+		}else if(data[0][0]+data[0][1]+data[0][2]==3){
+			System.out.println("plater 1 Win !");
+		}else if(data[1][0]+data[1][1]+data[1][2]==3){
+			System.out.println("plater 1 Win !");
+		}else if(data[2][0]+data[2][1]+data[2][2]==3){
+			System.out.println("plater 1 Win !");
+		}else if(data[0][2]+data[1][1]+data[2][0]==3){
+			System.out.println("plater 1 Win !");
+		}else if(data[0][0]+data[1][1]+data[2][2]==3){
+			System.out.println("plater 1 Win !");
+		}else if(data[0][0]+data[1][0]+data[2][0]==3){
+			System.out.println("plater 1 Win !");
+		}else if(data[0][1]+data[1][1]+data[2][1]==3){
+			System.out.println("plater 1 Win !");
+		}else if(data[0][2]+data[1][2]+data[2][2]==3){
+			System.out.println("plater 1 Win !");
+		}else{
+			System.out.println("False");
+		}
+		
 	}
 
 }
